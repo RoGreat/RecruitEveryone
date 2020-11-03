711db1b4c1a7e62927914d73d3c09558ea295b33
RoGreat <roguegreat@gmail.com>
2020-11-03 14:28:53 -0600

    Many fixes that address bad dialogs and other crashing. Added NoHarmony to overwrite the maxage... Hopefully does not mess to badly with the death by old age algorithm.

--- Patches/AgeModelMaxAgePatch.cs
+++ Patches/AgeModelMaxAgePatch.cs
@@ -1,24 +1,24 @@
-﻿using HarmonyLib;
-using SandBox.Source.Towns;
-using System;
-using System.Collections.Generic;
-using TaleWorlds.CampaignSystem;
-using TaleWorlds.CampaignSystem.Actions;
-using TaleWorlds.CampaignSystem.SandBox;
-using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
-using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors.Towns;
-using TaleWorlds.CampaignSystem.SandBox.GameComponents;
-using TaleWorlds.Core;
-using TaleWorlds.Localization;
+﻿//using HarmonyLib;
+//using SandBox.Source.Towns;
+//using System;
+//using System.Collections.Generic;
+//using TaleWorlds.CampaignSystem;
+//using TaleWorlds.CampaignSystem.Actions;
+//using TaleWorlds.CampaignSystem.SandBox;
+//using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
+//using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors.Towns;
+//using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+//using TaleWorlds.Core;
+//using TaleWorlds.Localization;
 
-namespace RecruitEveryone
-{
-    [HarmonyPatch(typeof(DefaultAgeModel), "MaxAge", MethodType.Getter)]
-    internal class AgeModelMaxAge
-    {
-		private static void Postfix(ref int __result)
-        {
-            __result = 80;
-		}
-    }
-}
+//namespace RecruitEveryone
+//{
+//    [HarmonyPatch(typeof(DefaultAgeModel), "MaxAge", MethodType.Getter)]
+//    internal class AgeModelMaxAge
+//    {
+//		private static void Postfix(ref int __result)
+//        {
+//            __result = 80;
+//		}
+//    }
+//}
--- RECampaignBehavior.cs
+++ RECampaignBehavior.cs
@@ -3,6 +3,7 @@ using MountAndBlade.CampaignBehaviors;
 using SandBox;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Actions;
@@ -15,7 +16,7 @@ namespace RecruitEveryone
 {
     internal class RECampaignBehavior : CampaignBehaviorBase
     {
-        private static List<int> _hiredAgents;
+        private static List<int> _recruitedAgents;
 
         private List<CharacterObject> _companionTemplatesFemale;
 
@@ -23,11 +24,27 @@ namespace RecruitEveryone
 
         protected void AddDialogs(CampaignGameStarter starter)
         {
+            // Reset them as wanderers after game restarts
+            foreach (CharacterObject character in CharacterObject.All)
+            {
+                if (character.HeroObject != null)
+                {
+                    if (character.Occupation != Occupation.Wanderer && character.HeroObject.IsPlayerCompanion)
+                    {
+                        Trace.WriteLine(character.Name.ToString());
+                        AccessTools.Property(typeof(CharacterObject), "Occupation").SetValue(character, Occupation.Wanderer, null);
+                    }
+                }
+            }
+
+            // Skip the nonexistent introduction
+            starter.AddDialogLine("wanderer_skip_intro", "wanderer_preintroduction", "hero_main_options", "{=LUiQ6bpo}Very well, then. What is it?", new ConversationSentence.OnConditionDelegate(conversation_wanderer_skip_preintroduction_on_condition), null, 200, null);
+
             RecruitCharacter(starter, "tavernkeeper_talk");
             RecruitCharacter(starter, "tavernmaid_talk");
             RecruitCharacter(starter, "talk_bard_player");
             // CharacterHire(starter, "arena_master_talk");     Single person, awkward
-            // CharacterHire(starter, "ransom_broker_talk");    Crashes
+            // RecruitCharacter(starter, "ransom_broker_talk");    Crashes
             RecruitCharacter(starter, "taverngamehost_talk");
             RecruitCharacter(starter, "town_or_village_player");
             RecruitCharacter(starter, "weaponsmith_talk_player");
@@ -36,6 +53,20 @@ namespace RecruitEveryone
             // Haven't tested
             // CharacterHire(starter, "prison_guard_talk");
             // CharacterHire(starter, "castle_guard_talk");
+        }
+
+        private bool conversation_wanderer_skip_preintroduction_on_condition()
+        {
+            if (Hero.OneToOneConversationHero != null)
+            {
+                string stringId = Hero.OneToOneConversationHero.Template.StringId;
+                TextObject result;
+                if (!GameTexts.TryGetText("prebackstory", out result, stringId))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         private void RecruitCharacter(CampaignGameStarter starter, string conversation)
@@ -56,11 +87,11 @@ namespace RecruitEveryone
         private bool able_to_recruit()
         {
             int agent = Math.Abs(Campaign.Current.ConversationManager.OneToOneConversationAgent.GetHashCode());
-            if (_hiredAgents == null)
+            if (_recruitedAgents == null)
             {
-                _hiredAgents = new List<int>();
+                _recruitedAgents = new List<int>();
             }
-            else if (_hiredAgents.Contains(agent))
+            else if (_recruitedAgents.Contains(agent))
             {
                 return false;
             }
@@ -69,69 +100,57 @@ namespace RecruitEveryone
 
         private void recruit_character()
         {
-            int hiredAgent = Math.Abs(Campaign.Current.ConversationManager.OneToOneConversationAgent.GetHashCode());
-            _hiredAgents.Add(hiredAgent);
-
-            // Agents for some reason always have default weight and build bodyproperties
-            Agent agent = (Agent)MissionConversationHandler.Current.ConversationManager.OneToOneConversationAgent;
-            CharacterObject character = CharacterObject.OneToOneConversationCharacter;
-            RESubModule.Debug("Character: " + character.Age + "   Agent: " + agent.Age);
-
-            // Create companion action
-            Hero hero = HeroCreator.CreateSpecialHero(character, Settlement.CurrentSettlement, null, null, (int)agent.Age);
-
-            // Change body properties to what the agent is currently
-            BodyProperties agentBodyProperties = agent.BodyPropertiesValue;
-            // RESubModule.Debug("DynamicProperties: " + agentBodyProperties.DynamicProperties.ToString());
-
-            AccessTools.Property(typeof(Hero), "StaticBodyProperties").SetValue(hero, agentBodyProperties.StaticProperties);
-            // Weight and build are fixed at the maximum. Seems to be systemic.
-            //AccessTools.Property(typeof(Hero), "Weight").SetValue(hero, MBRandom.RandomFloatRanged(agentBodyProperties.DynamicProperties.Weight, character.GetBodyPropertiesMax().Weight));
-            //AccessTools.Property(typeof(Hero), "Build").SetValue(hero, MBRandom.RandomFloatRanged(character.StaticBodyPropertiesMin.Weight, character.GetBodyPropertiesMax().Weight));
-            //RESubModule.Debug("New Weight: " + agentBodyProperties.Weight + "   New Build: " + agentBodyProperties.Build);
-
-            //float minWeight = 0.3f;
-            //float minBuild = 0.3f;
-            //float maxWeight = 1.0f;
-            //float maxBuild = 1.0f;
-            // Beggar
-            //if (character.Name.GetID() == "HZRKBDyB")
-            //{
-            //    RESubModule.Debug("Beggar");
-            //    minWeight = 0.0f;
-            //    minBuild = 0.05f;
-            //    maxWeight = 0.6f;
-            //    maxBuild = 0.7f;
-            //}
-            // Tavern Maid
-            //if (character.Name.GetID() == "z6RMWTaA" || character.Name.GetID() == "X3KYysuv")
-            //{
-            //    RESubModule.Debug("Beauty");
-            //    maxWeight = 0.8f;
-            //    maxBuild = 0.7f;
-            //}
-            //hero.Weight = MBRandom.RandomFloatRanged(minWeight, maxWeight);
-            //hero.Build = MBRandom.RandomFloatRanged(minBuild, maxBuild);
-            // RESubModule.Debug(hero.Weight.ToString());
-            // RESubModule.Debug(hero.Build.ToString());
-
-            // Different skill templates for each gender
-            _companionTemplatesFemale = new List<CharacterObject>(from x in CharacterObject.Templates where x.Occupation == Occupation.Wanderer && x.IsFemale select x);
-            _companionTemplatesMale = new List<CharacterObject>(from x in CharacterObject.Templates where x.Occupation == Occupation.Wanderer && !x.IsFemale select x);
-            if (character.IsFemale)
+            Hero hero; 
+            if (Hero.OneToOneConversationHero == null)
             {
-                int count = _companionTemplatesFemale.Count;
-                int num = MBRandom.RandomInt(count - 1);
-                Campaign.Current.GetCampaignBehavior<IHeroCreationCampaignBehavior>().DeriveSkillsFromTraits(hero, _companionTemplatesFemale[num]);
+                int recruitedAgent = Math.Abs(Campaign.Current.ConversationManager.OneToOneConversationAgent.GetHashCode());
+                _recruitedAgents.Add(recruitedAgent);
+
+                // Agents for some reason always have default weight and build bodyproperties
+                Agent agent = (Agent)MissionConversationHandler.Current.ConversationManager.OneToOneConversationAgent;
+                CharacterObject character = CharacterObject.OneToOneConversationCharacter;
+                RESubModule.Debug("Character: " + character.Age + "   Agent: " + agent.Age);
+
+                // Create companion action
+                hero = HeroCreator.CreateSpecialHero(character, Settlement.CurrentSettlement, null, null, (int)agent.Age);
+
+                // Change body properties to what the agent is currently
+                BodyProperties agentBodyProperties = agent.BodyPropertiesValue;
+                RESubModule.Debug("DynamicProperties: " + agentBodyProperties.DynamicProperties.ToString());
+
+                AccessTools.Property(typeof(Hero), "StaticBodyProperties").SetValue(hero, agentBodyProperties.StaticProperties);
+
+                // Different skill templates for each gender
+                _companionTemplatesFemale = new List<CharacterObject>(from x in CharacterObject.Templates where x.Occupation == Occupation.Wanderer && x.IsFemale select x);
+                _companionTemplatesMale = new List<CharacterObject>(from x in CharacterObject.Templates where x.Occupation == Occupation.Wanderer && !x.IsFemale select x);
+                if (character.IsFemale)
+                {
+                    int count = _companionTemplatesFemale.Count;
+                    int num = MBRandom.RandomInt(count - 1);
+                    Campaign.Current.GetCampaignBehavior<IHeroCreationCampaignBehavior>().DeriveSkillsFromTraits(hero, _companionTemplatesFemale[num]);
+                }
+                else
+                {
+                    int count = _companionTemplatesMale.Count;
+                    int num = MBRandom.RandomInt(count - 1);
+                    Campaign.Current.GetCampaignBehavior<IHeroCreationCampaignBehavior>().DeriveSkillsFromTraits(hero, _companionTemplatesMale[num]);
+                }
+
+                // Remove agent after recruitment (prevent doppelgangers)
+                // Will crash if used outside of the tavern in most cases
+                // Causes crashing with ransom broker
+                if (CampaignMission.Current.Location != null && CampaignMission.Current.Location.StringId == "tavern")
+                {
+                    Location location = CampaignMission.Current.Location;
+                    location.RemoveLocationCharacter(location.GetLocationCharacter(agent.Origin));
+                }
             }
             else
             {
-                int count = _companionTemplatesMale.Count;
-                int num = MBRandom.RandomInt(count - 1);
-                Campaign.Current.GetCampaignBehavior<IHeroCreationCampaignBehavior>().DeriveSkillsFromTraits(hero, _companionTemplatesMale[num]);
+                hero = Hero.OneToOneConversationHero;
             }
 
-            // Companion finalizing
+            // Create companion from cheat menu, opting to use the built in actions
             // GiveGoldAction.ApplyBetweenCharacters(null, hero, 20000, true);
             //hero.HasMet = true;
             //hero.ChangeState(Hero.CharacterStates.Active);
@@ -139,20 +158,13 @@ namespace RecruitEveryone
             //AddHeroToPartyAction.Apply(hero, MobileParty.MainParty, true);
             //CampaignEventDispatcher.Instance.OnHeroCreated(hero, false);
 
-            AddCompanionAction.Apply(Clan.PlayerClan, hero);
-            AddHeroToPartyAction.Apply(hero, MobileParty.MainParty, true);
-
             // Change occupation accordingly
             AccessTools.Property(typeof(CharacterObject), "Occupation").SetValue(hero.CharacterObject, Occupation.Wanderer, null);
 
-            // Remove agent after recruitment (prevent doppelgangers)
-            // Will crash if used outside of the tavern in most cases
-            // Probably causes crashing with ransom broker
-            if (CampaignMission.Current.Location != null && CampaignMission.Current.Location.StringId == "tavern")
-            {
-                Location location = CampaignMission.Current.Location;
-                location.RemoveLocationCharacter(location.GetLocationCharacter(agent.Origin));
-            }
+            RESubModule.Debug(hero.CharacterObject.Occupation.ToString());
+
+            AddCompanionAction.Apply(Clan.PlayerClan, hero);
+            AddHeroToPartyAction.Apply(hero, MobileParty.MainParty, true);
         }
 
         public void OnSessionLaunched(CampaignGameStarter campaignGameStarter)
