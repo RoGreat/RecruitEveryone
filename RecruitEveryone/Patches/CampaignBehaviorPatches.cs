617bbb0527d055b2e2644fdfa6d6f7bba0566169
RoGreat <RogueGreat@gmail.com>
2022-08-07 02:16:45 -0500

    Add project files.

---
+++ RecruitEveryone/Patches/CampaignBehaviorPatches.cs
@@ -0,0 +1,80 @@
+﻿using HarmonyLib;
+using SandBox.CampaignBehaviors;
+using System;
+using TaleWorlds.CampaignSystem;
+
+namespace RecruitEveryone.Patches
+{
+    [HarmonyPatch]
+    internal class CampaignBehaviorPatches
+    {
+        [HarmonyReversePatch]
+        [HarmonyPatch(typeof(LordConversationsCampaignBehavior), "conversation_hero_hire_on_condition")]
+        private static bool conversation_hero_hire_on_condition_patch(object instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool conversation_hero_hire_on_condition()
+        {
+            return conversation_hero_hire_on_condition_patch(new LordConversationsCampaignBehavior());
+        }
+
+        [HarmonyReversePatch]
+        [HarmonyPatch(typeof(LordConversationsCampaignBehavior), "too_many_companions")]
+        private static bool too_many_companions_patch(object instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool too_many_companions()
+        {
+            Hero hero = Hero.OneToOneConversationHero;
+            bool ret;
+            ret = conversation_hero_hire_on_condition_patch(new LordConversationsCampaignBehavior());
+            if (hero.HeroState != Hero.CharacterStates.Disabled)
+            {
+                hero.ChangeState(Hero.CharacterStates.Disabled);
+            }
+            return ret;
+        }
+
+        [HarmonyReversePatch]
+        [HarmonyPatch(typeof(LordConversationsCampaignBehavior), "conversation_companion_hire_gold_on_condition")]
+        private static bool conversation_companion_hire_gold_on_condition_patch(object instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool conversation_companion_hire_gold_on_condition()
+        {
+            return conversation_companion_hire_gold_on_condition_patch(new LordConversationsCampaignBehavior());
+        }
+
+        [HarmonyReversePatch]
+        [HarmonyPatch(typeof(LordConversationsCampaignBehavior), "conversation_companion_hire_on_condition")]
+        private static bool conversation_companion_hire_on_condition_patch(object instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool conversation_companion_hire_on_condition()
+        {
+            return conversation_companion_hire_on_condition_patch(new LordConversationsCampaignBehavior());
+        }
+
+        [HarmonyReversePatch]
+        [HarmonyPatch(typeof(LordConversationsCampaignBehavior), "conversation_companion_hire_on_consequence")]
+        private static void conversation_companion_hire_on_consequence_patch(object instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void conversation_companion_hire_on_consequence()
+        {
+            Hero hero = Hero.OneToOneConversationHero;
+
+            conversation_companion_hire_on_consequence_patch(new LordConversationsCampaignBehavior());
+        }
+    }
+}
---
+++ RecruitEveryone/SubModule.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+using RecruitEveryone.Behaviors;
+
+namespace RecruitEveryone
+{
+    internal class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            new Harmony("mod.bannerlord.everyone.recruit").PatchAll();
+        }
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            base.OnGameStart(game, gameStarterObject);
+            if (game.GameType is Campaign)
+            {
+                CampaignGameStarter campaignGameStarter = (CampaignGameStarter)gameStarterObject;
+                campaignGameStarter.AddBehavior(new RecruitEveryoneCampaignBehavior());
+            }
+        }
+    }
+}
